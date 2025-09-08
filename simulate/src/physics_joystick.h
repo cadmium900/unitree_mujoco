42f7c5287fe9721d1db2d8f2e2658435d922036b
lupinjia <jiayasen021003@gmail.com>
2025-09-08 16:03:38 +0800

    added betop joystick support

--- simulate/src/physics_joystick.h
+++ simulate/src/physics_joystick.h
@@ -130,3 +130,45 @@ private:
 	std::unique_ptr<Joystick> js_;
 	int max_value_;
 };
+
+// BeTop USB joystick
+class BeTopJoystick : public unitree::common::UnitreeJoystick
+{
+public:
+    BeTopJoystick(std::string device, int bits = 15)
+	: unitree::common::UnitreeJoystick()
+	{
+		js_ = std::make_unique<Joystick>(device);
+		if(!js_->isFound()) {
+			std::cout << "Error: Joystick open failed." << std::endl;
+			exit(1);
+		}
+        max_value_ = 1 << (bits - 1);
+	}
+
+    void update() override
+    {
+        js_->getState();
+        back(js_->button_[6]); // select button
+        start(js_->button_[7]);
+        LB(js_->button_[4]);
+        RB(js_->button_[5]);
+        A(js_->button_[0]);
+        B(js_->button_[1]); 
+        X(js_->button_[2]);
+        Y(js_->button_[3]);
+        up(js_->axis_[7] < 0);
+        down(js_->axis_[7] > 0);
+        left(js_->axis_[6] < 0);
+        right(js_->axis_[6] > 0);
+        LT(js_->axis_[2] > 0);
+        RT(js_->axis_[5] > 0);
+        lx(double(js_->axis_[0]) / max_value_);
+        ly(-double(js_->axis_[1]) / max_value_);
+        rx(double(js_->axis_[3]) / max_value_);
+        ry(-double(js_->axis_[4]) / max_value_);
+    }
+private:
+	std::unique_ptr<Joystick> js_;
+	int max_value_;
+};
